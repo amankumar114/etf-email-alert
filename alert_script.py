b6e28d91990f8ba9e1c95ad3338105bb5060e184
Aman Kumar <amankumarism@gmail.com>
2025-06-20 22:02:48 +0530

    Update alert_script.py

--- alert_script.py
+++ alert_script.py
@@ -25,7 +25,7 @@ LAST_BUY_FILE = 'last_buy_dates.json'
 
 EMAIL_SENDER = os.getenv("EMAIL")
 EMAIL_PASSWORD = os.getenv("PASS")
-EMAIL_RECEIVER = ["amankumarism@gmail.com", "amaniitism0405@gmail.com", ""]
+EMAIL_RECEIVER = ["amaniitism0405@gmail.com", "Muskanrohada432@gmail.com"]
 
 # Setup logging
 logging.basicConfig(
